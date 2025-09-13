b1f34ae4ffa39e67d58b4a2032132752cf806037
tatyana86 <Krivonogova_1999@mail.ru>
2025-09-13 21:02:31 +0400

    add solution of task_09

---
+++ src/main/task_09/ThreadExample.java
@@ -0,0 +1,30 @@
+package main.task_09;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadExample {
+    private static AtomicInteger counter = new AtomicInteger(0); // атомарный счетчик
+
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.incrementAndGet();
+            }
+        };
+
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Counter: " + counter.get());
+    }
+}
