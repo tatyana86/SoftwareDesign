b4011361f5be110b17d5c5cb4c4b309dbdc70b66
tatyana86 <Krivonogova_1999@mail.ru>
2025-09-16 20:34:34 +0400

    add solution of task_10

---
+++ src/main/task_10/SimpleMultiThreadProcessing.java
@@ -0,0 +1,20 @@
+package main.task_10;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SimpleMultiThreadProcessing {
+    private static final int SIZE = 1000000;
+    private static final int[] data = new int[SIZE];
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            data[i] = random.nextInt(100);
+        }
+
+        int sum = Arrays.stream(data).parallel().sum();
+
+        System.out.println("Sum of all elements: " + sum);
+    }
+}
